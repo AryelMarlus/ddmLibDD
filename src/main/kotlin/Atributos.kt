b5bfce3ce8dd51f37df3ca2987b1f993221d25db
AryelMarlus <aryelmarlus@gmail.com>
2024-09-11 13:21:31 -0300

    simple return Atributos.kt

--- src/main/kotlin/Atributos.kt
+++ src/main/kotlin/Atributos.kt
@@ -1,6 +1,8 @@
 package org.example
 
-class Atributos(val pontosDisponiveis: Int = 27) {
+class Atributos(var pontosDisponiveis: Int = 27) {
 
-
+    fun setForca(forca: Int): Int{
+        return forca
+    }
 }
